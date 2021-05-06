90c86d4f5cc39205312b4f98f8b25412d64a87d6
Satyakrishna Jasti <satyajasti9@gmail.com>
2021-05-06 16:07:35 +0530

    updated

--- src/app/data/data.component.ts
+++ src/app/data/data.component.ts
@@ -117,7 +117,7 @@ export class DataComponent implements OnInit {
     if (res == true) {
       this.ds.delete(id).subscribe(
         data => {
-          console.log("Successfully delete the record");
+          alert("Successfully delete the record");
           console.log(data);
         }
       )
